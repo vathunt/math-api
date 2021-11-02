4df6fdc251d5be8c036a7d00d642b8b5a22dcf2f
vathunt <fathan_46q12@ymail.com>
2021-11-02 14:00:32 +0700

    Menjalankan Math API HTTP Server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+const createServer = require('./createServer');
+const FigureCalculator = require('./FigureCalculator');
+const MathBasic = require('./MathBasic');
+
+const start = async () => {
+  const figureCalculator = new FigureCalculator(MathBasic);
+  const server = createServer({
+    mathBasic: MathBasic,
+    figureCalculator,
+  });
+
+  await server.start();
+  console.log(`Server start at ${server.info.uri}`);
+};
+
+start();
